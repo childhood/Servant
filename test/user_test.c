355fe457c03e743d1f74a5e62698c48b37d1280f
Renato Mascarenhas Costa <renato.mascosta@gmail.com>
2011-04-21 23:50:45 -0300

    Updated user tests for UNORDERED_LIST behavior

--- test/user_test.c
+++ test/user_test.c
@@ -78,7 +78,64 @@ int main(int argc, char* argv[]) {
 
 	free(servant_users); 
 
-	printf("\nTests are done. No errors.\n");
+	printf("\n\nNow using UNORDERED_LIST behavior.\n");
+	initialize(&servant_users, UNORDERED_LIST);
+
+	strncpy(u1.username, "user1", sizeof(u1.username) -1);
+	u1.username[31] = '\0';
+	strncpy(u1.password, "password1", sizeof(u1.password) - 1);
+	u1.password[31] = '\0';
+
+	strncpy(u2.username, "user2", sizeof(u2.username) -1);
+	u2.username[31] = '\0';
+	strncpy(u2.password, "password2", sizeof(u2.password) - 1);
+	u2.password[31] = '\0';
+
+	strncpy(u3.username, "user3", sizeof(u3.username) -1);
+	u3.username[31] = '\0';
+	strncpy(u3.password, "password3", sizeof(u3.password) - 1);
+	u3.password[31] = '\0';
+
+	strncpy(u4.username, "user4", sizeof(u4.username) -1);
+	u4.username[31] = '\0';
+	strncpy(u4.password, "password4", sizeof(u4.password) - 1);
+	u4.password[31] = '\0';
+
+	strncpy(u5.username, "user5", sizeof(u5.username) -1);
+	u5.username[31] = '\0';
+	strncpy(u5.password, "password5", sizeof(u5.password) - 1);
+	u5.password[31] = '\0';
+
+	strncpy(u6.username, "user6", sizeof(u6.username) -1);
+	u6.username[31] = '\0';
+	strncpy(u6.password, "password6", sizeof(u6.password) - 1);
+	u6.password[31] = '\0';
+
+
+	add(servant_users, u4);
+	add(servant_users, u2);
+	add(servant_users, u3);
+	add(servant_users, u1);
+	add(servant_users, u5); 
+
+	print_users(servant_users);
+
+	print_if_has_user(servant_users, u1);
+	print_if_has_user(servant_users, u3);
+	print_if_has_user(servant_users, u5);
+	print_if_has_user(servant_users, u6);
+
+	printf("\nRemoving users 1, 3 and 5...\n");
+	remove_user(servant_users, u1);
+	remove_user(servant_users, u5);
+	remove_user(servant_users, u3);
+
+	print_users(servant_users);
+
+	free(servant_users); 
+
+
+	printf("\n\nTests are done. No errors.\n");
 
 	return 0;
 }
